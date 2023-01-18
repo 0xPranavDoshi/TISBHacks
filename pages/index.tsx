abb4e179103d8f4b1713a46cc57ec1f0c77f2280
Pranav Doshi <pranav.rdoshi@gmail.com>
2023-01-18 17:14:10 +0530

    fix: updated faqs

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,19 +6,120 @@ import Navbar from '@/components/Navbar'
 import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io'
 import { useState } from 'react'
 import Footer from '@/components/Footer'
+import FAQ, { FAQEnum, FAQProps } from '@/components/FAQ'
 
 const inter = Inter({ subsets: ['latin'] })
 
-const enum FAQ {
-  NONE = '',
-  Q1 = 'How do I sign up?',
-  Q2 = 'Is there a fee for the event?',
-  Q3 = 'What is the team size?',
-  Q4 = 'Is the hackathon offline or online?',
-}
-
 export default function Home() {
-  const [faq, setFaq] = useState<FAQ>(FAQ.NONE)
+  const faqs: FAQProps[] = [
+    {
+      faqVal: FAQEnum.Q1,
+      question: 'What are the benefits of participating?',
+      answer:
+        'Learn new skills, add a nation-wide event to your Resume, network with the best programmers in India, and win cash prizes worth Rs. 65000. All while meeting great people, having great food, and having great fun!',
+    },
+    {
+      faqVal: FAQEnum.Q2,
+      question: 'Is there support available?',
+      answer:
+        'The environment at these events is designed specifically to encourage everyone to have fun and help each other. If you’re ever stuck on anything, speak to a volunteer or a HOD who will help you. The primary purpose of the hackathon is to learn, so don’t be afraid to ask!',
+    },
+    {
+      faqVal: FAQEnum.Q3,
+      question: 'Do you need coding experience to participate?',
+      answer:
+        'No! If you’re new to programming, feel free to participate in the UI Development Category.',
+    },
+    {
+      faqVal: FAQEnum.Q4,
+      question: 'Is the hackathon open to anyone?',
+      answer:
+        'We have two categories; one for ages 10-13 and another for ages 13-18.',
+    },
+    {
+      faqVal: FAQEnum.Q5,
+      question: 'Are there Cash Prizes?',
+      answer: (
+        <>
+          <p>
+            Yes! There are cash prizes worth Rs. 65’000 in total distributed as
+            follows:
+          </p>
+          <br />
+          <p>
+            <b>Coding Category:</b> First Place = 15’000, Second Place = 10’000,
+            Third Place = 5000
+          </p>
+          <p>
+            <b>UX Design Category:</b> First Place = 15’000, Second Place =
+            10’000, Third Place = 5000
+          </p>
+        </>
+      ),
+    },
+    {
+      faqVal: FAQEnum.Q6,
+      question:
+        'I have another question that is not answered in your FAQs, what should I do?',
+      answer: 'Please contact us at hackathon@tisb.ac.in for further queries.',
+    },
+    {
+      faqVal: FAQEnum.Q7,
+      question: 'What about confidentiality of information?',
+      answer:
+        'If you don’t want to distribute your code base that is ok, if you don’t want to present your project, then a hackathon might not be the ideal place for you to work on that idea. We will take photos at the event and you will sign a document that allows us to use those pictures for social media. So if you don’t want to be on any pictures please tell us beforehand!',
+    },
+    {
+      faqVal: FAQEnum.Q8,
+      question: 'Do all team members need to be present in person?',
+      answer:
+        'Yes, every team member should be present. If a team member cannot be present during the pitch or can only come later please tell us and we will find a way to manage!',
+    },
+    {
+      faqVal: FAQEnum.Q9,
+      question: 'What is the Program/Timetable?',
+      answer:
+        'The schedule of events will be decided and notified to all participants shortly.',
+    },
+    {
+      faqVal: FAQEnum.Q10,
+      question: 'What should I bring?',
+      answer:
+        'We suggest you to bring your laptops and mobile phones. You can bring anything else as long as it complies with the code of conduct.',
+    },
+    {
+      faqVal: FAQEnum.Q11,
+      question: 'Will there be wireless internet access?',
+      answer: 'Yes, there will be free internet access to all participants.',
+    },
+    {
+      faqVal: FAQEnum.Q12,
+      question: 'Will there be food available?',
+      answer: 'Yes, there will be unlimited snacks available for participants!',
+    },
+    {
+      faqVal: FAQEnum.Q13,
+      question: 'How are the winners selected?',
+      answer: (
+        <>
+          There will be an independent Jury comprised of experienced judges
+          selecting the winners with a rating system:{' '}
+          <b>
+            (Implementability, financial benefits, presentation skills,
+            creativity)
+          </b>
+        </>
+      ),
+    },
+    {
+      faqVal: FAQEnum.Q14,
+      question: 'What is the dress code for the event?',
+      answer:
+        'While there is no official dress code for the event, we wish to remind participants to dress appropriately.',
+    },
+  ]
+
+  const [faq, setFaq] = useState<FAQEnum>(FAQEnum.NONE)
 
   return (
     <>
@@ -97,133 +198,18 @@ export default function Home() {
         <div className='flex items-start justify-between w-screen h-auto bg-primary'>
           <div className='flex flex-col items-start justify-start p-8'>
             <h1 className='mb-8 text-6xl font-semibold'>FAQs</h1>
-            <div className='pl-4 pr-4 rounded-md w-[45rem] py-4 flex flex-col justify-start items-start bg-gray'>
-              <div
-                className='flex items-center justify-between w-full cursor-pointer'
-                onClick={() =>
-                  faq === FAQ.Q1 ? setFaq(FAQ.NONE) : setFaq(FAQ.Q1)
-                }
-              >
-                <p className='text-xl font-medium text-white'>
-                  How do I sign up?
-                </p>
-                {faq === FAQ.Q1 ? (
-                  <IoIosArrowUp size='20' className='text-tertiary' />
-                ) : (
-                  <IoIosArrowDown size='20' className='text-tertiary' />
-                )}
-              </div>
-
-              {faq === FAQ.Q1 && (
-                <p className='mt-4 text-tertiary'>
-                  You can register for the event using this{' '}
-                  <a
-                    className='text-blue-400 underline cursor-pointer'
-                    target='_blank'
-                    rel='noreferrer'
-                    href='https://5h574blbwuh.typeform.com/to/eJppZjWY'
-                  >
-                    link
-                  </a>
-                  .
-                </p>
-              )}
-            </div>
-
-            <div className='mt-4 pl-4 pr-4 rounded-md w-[45rem] py-4 flex flex-col justify-start items-start bg-gray'>
-              <div
-                className='flex items-center justify-between w-full cursor-pointer'
-                onClick={() =>
-                  faq === FAQ.Q2 ? setFaq(FAQ.NONE) : setFaq(FAQ.Q2)
-                }
-              >
-                <p className='text-xl font-medium text-white'>
-                  Is there a fee for the event?
-                </p>
-                {faq === FAQ.Q2 ? (
-                  <IoIosArrowUp size='20' className='text-tertiary' />
-                ) : (
-                  <IoIosArrowDown size='20' className='text-tertiary' />
-                )}
-              </div>
-
-              {faq === FAQ.Q2 && (
-                <p className='mt-4 text-tertiary'>
-                  No, there is no registration fee for the event.
-                </p>
-              )}
-            </div>
-
-            <div className='mt-4 pl-4 pr-4 rounded-md w-[45rem] py-4 flex flex-col justify-start items-start bg-gray'>
-              <div
-                className='flex items-center justify-between w-full cursor-pointer'
-                onClick={() =>
-                  faq === FAQ.Q3 ? setFaq(FAQ.NONE) : setFaq(FAQ.Q3)
-                }
-              >
-                <p className='text-xl font-medium text-white'>
-                  What is the team size?
-                </p>
-                {faq === FAQ.Q3 ? (
-                  <IoIosArrowUp size='20' className='text-tertiary' />
-                ) : (
-                  <IoIosArrowDown size='20' className='text-tertiary' />
-                )}
-              </div>
-
-              {faq === FAQ.Q3 && (
-                <p className='mt-4 text-tertiary'>
-                  Teams can be composed of <b>1-6 members (inclusive)</b>. If
-                  you are having struggle finding team members, you can email us
-                  and we will try and connect you with other students.
-                </p>
-              )}
-            </div>
-
-            <div className='mt-4 pl-4 pr-4 rounded-md w-[45rem] py-4 flex flex-col justify-start items-start bg-gray'>
-              <div
-                className='flex items-center justify-between w-full cursor-pointer'
-                onClick={() =>
-                  faq === FAQ.Q4 ? setFaq(FAQ.NONE) : setFaq(FAQ.Q4)
-                }
-              >
-                <p className='text-xl font-medium text-white'>
-                  Is the hackathon offline or online?
-                </p>
-                {faq === FAQ.Q4 ? (
-                  <IoIosArrowUp size='20' className='text-tertiary' />
-                ) : (
-                  <IoIosArrowDown size='20' className='text-tertiary' />
-                )}
-              </div>
-
-              {faq === FAQ.Q4 && (
-                <div className='mt-4 text-tertiary'>
-                  <p>
-                    The first week of the hackathon will be held online, from{' '}
-                    <b>January 28th - Feb 3rd</b>. During this time, you can
-                    work on your project and engage with other participants on
-                    the discord server. Several webinars and workshops will also
-                    be held online.
-                  </p>
-                  <br />
-                  <p>
-                    <b>February 4th</b>, the final day of the hackathon, will be
-                    held in hybrid mode; this means you have the option to come
-                    to TISB physically if you want, although this is not a
-                    requirement and will not disadvantage you.
-                  </p>
-                  <br />
-                  <p>
-                    If you do choose to come to school physically, you will have
-                    the opportunity to participate in additional mini-events,
-                    such as a crisis event, competitive coding and gaming. You
-                    will also have the option to present your project to the
-                    judges in-person.
-                  </p>
-                </div>
-              )}
-            </div>
+            {faqs.map((faqObj: FAQProps) => {
+              return (
+                <FAQ
+                  faq={faq}
+                  setFaq={setFaq}
+                  key={faqObj.question}
+                  question={faqObj.question}
+                  faqVal={faqObj.faqVal}
+                  answer={faqObj.answer}
+                />
+              )
+            })}
 
             <h3 className='mt-16 mb-8 text-4xl font-semibold' id='sponsors'>
               Sponsors
@@ -251,11 +237,18 @@ export default function Home() {
             </div>
           </div>
 
-          <img
-            src='/images/secondary_bg.png'
-            className='h-full select-none'
-            alt=''
-          />
+          <div className='flex flex-col items-center justify-center'>
+            <img
+              src='/images/secondary_bg.png'
+              className='h-full select-none'
+              alt=''
+            />
+            <img
+              src='/images/secondary_bg.png'
+              className='h-full select-none scale-y-[-1]'
+              alt=''
+            />
+          </div>
         </div>
 
         <Footer />
