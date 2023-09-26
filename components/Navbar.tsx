cd91fa6c1f35545f64a32e8a143d8ec8ffdd9f3b
Pranav Doshi <pranav.rdoshi@gmail.com>
2023-09-26 15:06:46 +0530

    fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -80,7 +80,7 @@ const Navbar = () => {
         </p>
         <div
           onClick={() => {}}
-          className="px-6 py-2 mr-8 rounded-full cursor-pointer sm:mr-12 bg-gradient-to-b from-gradientTop to-gradientBottom"
+          className="hidden sm:block px-6 py-2 mr-8 rounded-full cursor-pointer sm:mr-12 bg-gradient-to-b from-gradientTop to-gradientBottom"
         >
           Registrations Opening Soon!
         </div>
